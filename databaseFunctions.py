17a41e98085de55bd750f1c82c7b16b7a4e21827
pharsos <chrisjbaker@gmail.com>
2015-01-08 23:32:37 -0500

    Code Cleanup
    Made timestamps mode comparator-friendly on the back end. Removed
    unnecessary defs

--- databaseFunctions.py
+++ databaseFunctions.py
@@ -2,7 +2,7 @@ import sqlite3
 import sys
 import datetime
 from pprint import pprint
-from time import gmtime, strftime
+from time import time, gmtime, strftime, mktime, localtime
 sys.dont_write_bytecode = True
 ROWS = {'1':'A',
         '2':'B',
@@ -17,60 +17,27 @@ ROWS = {'1':'A',
         '11':'K',
         '12':'L'}
 
-
-def getTables():
-  conn = sqlite3.connect("racks.db")
-  tables = conn.cursor()
-  tables.execute('select name from sqlite_master where type=\'table\'')
-  rows = []
-  for row in tables:
-    rows.append(row)
-  conn.close()
-  return rows
-getTables()
-
-
-def newDay():
-  tables = getTables()
-  needNewDay = True
-  today = datetime.datetime.now().strftime("%a%m%d%Y")
-  for table in tables:
-    if table == today:
-      needNewDay = False
-  if not needNewDay:
-    return today  
-  db = sqlite3.connect('racks.db')
-  cursor = db.cursor()
-  day = datetime.datetime.now().strftime("%A")
-  cursor.execute("""CREATE TABLE IF NOT EXISTS """ + today + """(id INTEGER PRIMARY KEY, accn TEXT, rackNum TEXT, date TEXT, timeFiled TEXT, col TEXT, row TEXT)""")
-  db.close()
-  return today
-newDay()
-
-
 def lastFiled():
-  today = str(newDay())
   db = sqlite3.connect('racks.db')
-  cursor = db.execute('SELECT max(id) FROM ' + today)
+  cursor = db.execute('SELECT max(id) FROM tube_data')
   max_id = cursor.fetchone()[0]
   if max_id is None:
     return None
   else:
-    cursor = db.execute('SELECT rackNum, col, row, accn FROM ' + today + ' where id is ' + str(max_id))
+    cursor = db.execute('SELECT rackNum, col, row, accn FROM tube_data where id is ' + str(max_id))
     lastFiled = cursor.fetchone()
     return lastFiled
 
 def locateNext():
-  today = str(newDay())
   db = sqlite3.connect('racks.db')
-  cursor = db.execute('SELECT max(id) FROM ' + today)
+  cursor = db.execute('SELECT max(id) FROM tube_data')
   max_id = cursor.fetchone()[0]
   if max_id is None:
     rackNum = 1
     col = 0
     row = 1
   else:
-    cursor = db.execute('SELECT rackNum, col, row FROM ' + today + ' where id is ' + str(max_id))
+    cursor = db.execute('SELECT rackNum, col, row FROM tube_data where id is ' + str(max_id))
     lastRow = cursor.fetchone()
     rackNum = int(lastRow[0])
     col = int(lastRow[1])
@@ -84,7 +51,7 @@ def locateNext():
     row += 1
   elif col != 6:
     col += 1
-  nextSpot = [str(rackNum), str(col), str(row), today[:3]]
+  nextSpot = [str(rackNum), str(col), str(row)]
   db.close()
   return nextSpot
   
@@ -98,22 +65,23 @@ def tuple_without(original_tuple, element_to_remove):
 def findAccn(accn):
   db = sqlite3.connect('racks.db')
   cursor = db.cursor()
-  today = newDay()
-  yesterday = (datetime.date.today() - datetime.timedelta(1)).strftime("%a%m%d%Y")
-  twoDaysAgo = (datetime.date.today() - datetime.timedelta(2)).strftime("%a%m%d%Y")
+  today = int(mktime(datetime.date.today().timetuple()))
+  yesterday = int(mktime((datetime.date.today() - datetime.timedelta(1)).timetuple()))
+  twoDaysAgo = int(mktime((datetime.date.today() - datetime.timedelta(2)).timetuple()))
+
+  print "Today", today
 
   dates = [today, yesterday, twoDaysAgo]
   rows = []
   for date in dates:
-    day = date[:3]
     try:
-      cursor.execute('SELECT id, rackNum, col, row FROM ' +date+ " WHERE accn == '"+accn+"' ORDER BY id DESC")
-    except sqlite3.OperationalError:
-      print "SQLite DB locked"
+      cursor.execute("SELECT id, rackNum, col, row FROM tube_data WHERE accn == '"+accn+"' AND timeFiled >= datetime(" + str(date) + ", 'unixepoch', 'localtime') AND timeFiled <= datetime(" + str(date + 86399) + ", 'unixepoch', 'localtime') ORDER BY id DESC")
+    except sqlite3.Error as e:
+      print "SQLite Error:", e.args[0]
     else:
       for thing in cursor:
         # print thing
-        rows.append(thing + (day,))
+        rows.append(thing + (strftime("%m%d%y", localtime(date)),))
 #  db.close()
   return rows
 
@@ -122,18 +90,18 @@ def findAll():
 #  print accn
   db = sqlite3.connect('racks.db')
   cursor = db.cursor()
-  today = newDay()
-  yesterday = (datetime.date.today() - datetime.timedelta(1)).strftime("%a%m%d%Y")
-  twoDaysAgo = (datetime.date.today() - datetime.timedelta(2)).strftime("%a%m%d%Y")
+  today = int(mktime(datetime.date.today().timetuple()))
+  yesterday = int(mktime((datetime.date.today() - datetime.timedelta(1)).timetuple()))
+  twoDaysAgo = int(mktime((datetime.date.today() - datetime.timedelta(2)).timetuple()))
+  print "Today", today
 
   dates = [twoDaysAgo, yesterday, today]
   rows = []
   setRows = orderedset.OrderedSet()
   for date in dates:
     #Getting the Day string
-    day = date[:3]
     try:
-      cursor.execute('SELECT id, accn FROM ' +date+ " ORDER BY id ASC")
+      cursor.execute("SELECT id, accn FROM tube_data WHERE timeFiled >= datetime(" + str(date) + ", 'unixepoch', 'localtime') AND timeFiled <= datetime(" + str(date + 86399) + ", 'unixepoch', 'localtime')s ORDER BY id ASC")
     except sqlite3.OperationalError:
       print "SQLite DB locked"
     else:
@@ -155,12 +123,10 @@ def findAll():
 
 
 def fileAccn(accn):
-  today = newDay()
   loc = locateNext()
   db = sqlite3.connect('racks.db')
-  date = strftime("%m-%d", gmtime())
-  timeFiled = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-  cursor = db.execute("INSERT INTO " + today  + " (accn, rackNum, date, timeFiled, col, row) VALUES(?,?,?,?,?,?)", (accn, loc[0], date, timeFiled, loc[1], loc[2]))
+  timeFiled = datetime.datetime.now()
+  cursor = db.execute("INSERT INTO tube_data (accn, rackNum, timeFiled, col, row) VALUES(?,?,?,?,?)", (accn, loc[0], timeFiled, loc[1], loc[2]))
   db.commit()
   db.close()
 
@@ -170,15 +136,13 @@ def testDB():
   while test < 10:
     fileAccn(accn)
     test +=1
-  db = sqlite3.connect('racks.db')
-  today = datetime.datetime.now().strftime("%a%m%d%Y")
-  cursor = db.execute('SELECT max(id) FROM ' + today)
-  max_id = cursor.fetchone()[0]
+  pprint(findAccn(accn))
 
 if __name__ == '__main__':
+  db = sqlite3.connect('racks.db')
+  db.execute("CREATE TABLE IF NOT EXISTS tube_data (id INTEGER PRIMARY KEY, accn TEXT, rackNum TEXT, timeFiled TIMESTAMP, col TEXT, row TEXT)")
+  db.close()
   testDB()
-  fileAccn("test")
-  pprint(findAccn("test"))
 
 
 
